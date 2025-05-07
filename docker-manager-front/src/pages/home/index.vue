9ccc7918e77aebda0afd65b305de83de8490c1a7
kidoneself <8768611@qq.com>
2025-05-08 07:57:40 +0800

    优化数据看板

--- docker-manager-front/src/pages/home/index.vue
+++ docker-manager-front/src/pages/home/index.vue
@@ -5,7 +5,7 @@
       class="cover"
       :style="{
         filter: `blur(${backgroundBlur}px)`,
-        background: `url('/src/assets/background.png') no-repeat`,
+        background: `url(${backgroundImage}) no-repeat`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }"
@@ -51,8 +51,8 @@
               @keyup.enter="handleSearch"
             />
             <div class="search-icon" @click="handleSearch">
-              <svg viewBox="0 0 1024 1024" width="20" height="20">
-                <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6c3.2 3.2 8.4 3.2 11.6 0l43.6-43.5c3.2-3.2 3.2-8.4 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z" fill="currentColor"/>
+              <svg viewBox="0 0 1024 1024" width="20" height="20" fill="currentColor">
+                <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6c3.2 3.2 8.4 3.2 11.6 0l43.6-43.5c3.2-3.2 3.2-8.4 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"/>
               </svg>
             </div>
           </div>
@@ -248,6 +248,8 @@
 import { onMounted, onUnmounted, ref } from 'vue';
 import { MessagePlugin, FormRules, SubmitContext, UploadFile, DialogPlugin } from 'tdesign-vue-next';
 import { useRouter } from 'vue-router';
+// 导入背景图片
+import backgroundImage from '/src/assets/background.png';
 
 const router = useRouter();
 
@@ -438,13 +440,13 @@ const searchEngines = [
   {
     name: 'Google',
     value: 'google',
-    icon: '/src/assets/icon/google.svg',
+    icon: '/icon/google.svg',
     url: 'https://www.google.com/search?q=',
   },
   {
     name: '百度',
     value: 'baidu',
-    icon: '/src/assets/icon/baidu.svg',
+    icon: '/icon/baidu.svg',
     url: 'https://www.baidu.com/s?wd=',
   },
 ];
@@ -906,6 +908,7 @@ onUnmounted(() => {
   width: 1.3rem;
   height: 1.3rem;
   object-fit: contain;
+  display: block;
 }
 
 .engine-dropdown {
@@ -967,15 +970,17 @@ onUnmounted(() => {
   align-items: center;
   justify-content: center;
   transition: color 0.2s;
-}
-
-.search-icon svg {
-  width: 18px;
-  height: 18px;
+  width: 20px;
+  height: 20px;
 }
 
 .search-icon:hover {
   color: #5c9eff;
+}
+
+.search-icon svg {
+  width: 100%;
+  height: 100%;
 }
 
 .new-app-grid {
